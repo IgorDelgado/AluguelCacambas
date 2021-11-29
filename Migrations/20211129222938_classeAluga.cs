4077a4e05dcabde9c9b0f2c8716fe00ac276983c
Igor Delgado <cars.igor1@gmail.com>
2021-11-29 19:30:54 -0300

    migrationsclasseAluga

---
+++ Migrations/20211129222938_classeAluga.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aluguel.Migrations
+{
+    public partial class classeAluga : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Cacamba",
+                table: "Alugueis",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cacamba",
+                table: "Alugueis");
+        }
+    }
+}
